ed7b1079c57f34f792194622e344b5e34c493ffe
junghyeonsu <54893898+junghyeonsu@users.noreply.github.com>
2020-09-13 16:44:37 +0900

    start and clean code

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBFNRTVY-IuMd6NUpXW4teLne13R6q79c0",
+    authDomain: "clone-bce99.firebaseapp.com",
+    databaseURL: "https://clone-bce99.firebaseio.com",
+    projectId: "clone-bce99",
+    storageBucket: "clone-bce99.appspot.com",
+    messagingSenderId: "805454027549",
+    appId: "1:805454027549:web:db02a6f4a95aeb4ee11ff4",
+    measurementId: "G-EH396L1YM8"
+  };
